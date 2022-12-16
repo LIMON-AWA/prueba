e115547861b9b6c671554da58a733136ea0b21f1
LEMON <99194228+LIMON-AWA@users.noreply.github.com>
2022-12-16 13:47:00 -0500

    new changes

---
+++ src/test/java/com/curso/ecommerce/SpringEcommerceApplicationTests.java
@@ -0,0 +1,9 @@
+package com.curso.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEcommerceApplicationTests {
+
+}
